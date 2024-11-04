3b83ff3db5f415401a51740e809be4fe17aa9575
RaviRanjan <raviranjanjha118@gmail.com>
2024-11-04 21:10:58 +0530

    initial commit

---
+++ SuccessSail_Store/src/main/java/ravi/SuccessSailStoreApplication.java
@@ -0,0 +1,13 @@
+package ravi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuccessSailStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuccessSailStoreApplication.class, args);
+	}
+
+}
---
+++ SuccessSail_Store/src/test/java/ravi/SuccessSailStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ravi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuccessSailStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
